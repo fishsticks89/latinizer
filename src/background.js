49a6d65824a09298d56871d7ae2982600f7c28ca
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2021-04-16 15:43:20 -0700

    updated plan, background.js is successfully called

---
+++ src/background.js
@@ -0,0 +1,20 @@
+chrome.runtime.onMessage.addListener(
+    function(iden) {
+        
+        var keyboardEvent = document.createEvent('KeyboardEvent');
+        var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
+        keyboardEvent[initMethod](
+            'keydown', // event type: keydown, keyup, keypress
+            true, // bubbles
+            true, // cancelable
+            window, // view: should be window
+            false, // ctrlKey
+            false, // altKey
+            false, // shiftKey
+            false, // metaKey
+            0, // keyCode: unsigned long - the virtual key code, else 0
+            iden, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
+        );
+        document.dispatchEvent(keyboardEvent);
+    }
+  );
--- src/sendit.js
+++ src/sendit.js
@@ -2,26 +2,10 @@ var char = ["&#x0101", "&#x0113", "&#x012B", "&#x014D", "&#x016B"];
 var id = ["0101", "00113", "0012B", "0014D", "0016B"];
 
 function macronize(iden) {
-    window.close();
-    
-    var keyboardEvent = document.createEvent('KeyboardEvent');
-    var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
-  
-    keyboardEvent[initMethod](
-      'keydown', // event type: keydown, keyup, keypress
-      true, // bubbles
-      true, // cancelable
-      window, // view: should be window
-      false, // ctrlKey
-      false, // altKey
-      false, // shiftKey
-      false, // metaKey
-      0, // keyCode: unsigned long - the virtual key code, else 0
-      iden, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
-    );
-    document.dispatchEvent(keyboardEvent);
+    chrome.runtime.sendMessage({macronize: iden});
+    // window.close();
 }
 
 for (let i = 0; i < char.length; i++) {
-  id[i].onclick = "macronize(id[" + i + "])";
+  document.getElementById(id[i]).addEventListener("click", () => macronize(id[i]));
 }
