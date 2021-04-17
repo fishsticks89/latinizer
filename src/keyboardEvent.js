c93862ccd5a70b9fc133ec56c77a070adf5057cd
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2021-04-17 15:04:09 -0700

    keyboardEvent Injects!

--- src/keyboardEvent.js
+++ src/keyboardEvent.js
@@ -1,23 +1,22 @@
-export function listenAndSend() {
-    chrome.runtime.onMessage.addListener(
-        function(identifier) {
-            
-            var keyboardEvent = document.createEvent('KeyboardEvent');
-            var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
-            keyboardEvent[initMethod](
-                'keydown', // event type: keydown, keyup, keypress
-                true, // bubbles
-                true, // cancelable
-                window, // view: should be window
-                false, // ctrlKey
-                false, // altKey
-                false, // shiftKey
-                false, // metaKey
-                0, // keyCode: unsigned long - the virtual key code, else 0
-                identifier.identifier, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
-            );
-            document.dispatchEvent(keyboardEvent);
-            sendResponse(1);
-        }
-    );
-}
+chrome.runtime.onMessage.addListener(
+    function(identifier) {
+        
+        var keyboardEvent = document.createEvent('KeyboardEvent');
+        var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
+        keyboardEvent[initMethod](
+            'keydown', // event type: keydown, keyup, keypress
+            true, // bubbles
+            true, // cancelable
+            window, // view: should be window
+            false, // ctrlKey
+            false, // altKey
+            false, // shiftKey
+            false, // metaKey
+            0, // keyCode: unsigned long - the virtual key code, else 0
+            identifier.identifier, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
+        );
+        document.dispatchEvent(keyboardEvent);
+        sendResponse(1);
+        return(1);
+    }
+);
